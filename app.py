ae8038c20356c77097e1bd026c65695a2cb5e4e2
Brandon Olynyk <brandontab2004@gmail.com>
2023-08-17 15:49:23 -0400

    name and media link scraped from embed

--- app.py
+++ app.py
@@ -12,9 +12,21 @@ async def on_ready():
     print(f"{bot.user} is ready and online!")
 
 @bot.slash_command(name='grab', description='Grabs all images from embeds in a specific channel')
-async def grab(ctx, channel:discord.TextChannel):
-    
-    
-    await ctx.respond(channel.last_message,ephemeral=True)
+async def grab(ctx, channel:discord.TextChannel, limit:int):
+    try:
+        messages = await channel.history(limit=limit).flatten()
+
+        message_texts = []
+        for message in messages:
+            #text = await channel.fetch_message(message.id)
+            message_texts.append(message.embeds[0].author)
+            message_texts.append(message.embeds[0].image)
+        print(message_texts)
+        await ctx.respond("Check log",ephemeral=True)
+    except Exception as e:
+        if '403' in str(e):
+            await ctx.respond(f'The bot does not have access to the channel "{channel.name}".',ephemeral=True)
+        else:        
+            await ctx.respond(f'Something went wrong when accessing the channel: {e}',ephemeral=True)
 
 bot.run(os.getenv("BOT_TOKEN"))
