fd5d0413638d291f7a8cd4772d8a1bc3d6383671
Brandon Olynyk <brandontab2004@gmail.com>
2023-08-17 12:55:48 -0400

    Basic Bot Working and Grabbing Channels

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-print("Hello World!")
-print("Nothing here yet...")
+import discord
+import dotenv
+import os
+import asyncio
+
+dotenv.load_dotenv()
+bot = discord.Bot(command_prefix='xl', description='A simple bot.')
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+@bot.slash_command(name='grab', description='Grabs all images from embeds in a specific channel')
+async def grab(ctx, channel:discord.TextChannel):
+    
+    
+    await ctx.respond(channel.last_message,ephemeral=True)
+
+bot.run(os.getenv("BOT_TOKEN"))
